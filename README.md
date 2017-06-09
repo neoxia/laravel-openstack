91c6c1784dd231afbe3d27c2e5f6d1eda95bf5c7
alexandre-butynski <alexandre.butynski@neoxia.com>
2017-06-09 16:08:10 +0200

    First version of OpenStackServiceProvider

---
+++ README.md
@@ -0,0 +1,41 @@
+[![Latest Stable Version](http://img.shields.io/github/release/neoxia/laravel-openstack.svg)](https://packagist.org/packages/neoxia/laravel-openstack)
+
+## Laravel OpenStack
+
+This package provide a service provider to add the "openstack" driver to Laravel Storage.
+
+## Installation
+
+In order to install this package, add `neoxia/laravel-openstack` in `composer.json`.
+
+```JS
+"require": {
+    "neoxia/laravel-openstack": "1.0.*"
+},
+```
+
+And add the service provider in `config/app.php`.
+
+```PHP
+Neoxia\Filesystem\OpenstackServiceProvider::class,
+```
+
+## Configuration
+
+To configure a new Laravel storage disk on OpenStack, provide a configuration like this one in `config/filesystems.php`
+
+```PHP
+'disks' => [
+
+    'openstack' => [
+        'driver'      => 'openstack',
+        'username'    => env('OS_USERNAME', ''),
+        'password'    => env('OS_PASSWORD', ''),
+        'container'   => env('OS_CONTAINER', ''),
+        'tenant_name' => env('OS_TENANT_NAME', ''),
+        'endpoint'    => env('OS_ENDPOINT', ''),
+        'region'      => env('OS_REGION', ''),
+    ],
+
+],
+```
---
+++ src/Neoxia/LaravelOpenStack/OpenStackServiceProvider.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Neoxia\LaravelOpenStack;
+
+use Illuminate\Support\ServiceProvider;
+use League\Flysystem\Filesystem;
+use League\Flysystem\Rackspace\RackspaceAdapter;
+use OpenCloud\OpenStack;
+
+class OpenStackServiceProvider extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $filesystem = $this->app->make('filesystem');
+
+        $filesystem->extend('openstack', function($app, $config) {
+            $client = new OpenStack($config['endpoint'], [
+                'username' => $config['username'],
+                'password' => $config['password'],
+                'tenantName' => $config['tenant_name'],
+            ]);
+
+            $store = $client->objectStoreService('swift', $config['region']);
+            $container = $store->getContainer($config['container']);
+
+            return new Filesystem(new RackspaceAdapter($container));
+        });
+    }
+
+    /**
+     * Register bindings in the container.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
